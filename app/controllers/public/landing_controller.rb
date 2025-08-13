99da2098e5e5bc56967021d1c7aa321f32ef98a2
dm0lz <ducrouxolivier@gmail.com>
2025-08-13 22:39:05 +0200

    fix

--- app/controllers/public/landing_controller.rb
+++ app/controllers/public/landing_controller.rb
@@ -1,5 +1,5 @@
 class Public::LandingController < Public::BaseController
   def index
-    @latest_block = EthereumBlock.order(created_at: :desc).limit(1).first
+    @latest_block = EthereumBlock.where.not(summary: nil).order(created_at: :desc).limit(1).first
   end
 end
